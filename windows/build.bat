e1b668151108816b02924a493d0a253e2341add6
matyas5 <orka770@seznam.cz>
2020-03-13 18:26:22 +0100

    windows: Set default msys location to C drive

--- windows/build.bat
+++ windows/build.bat
@@ -1,5 +1,5 @@
 @echo off
-set msys=S:\msys64\
+set msys=C:\msys64\
 echo The MSYS2 directroy is set to: %msys%. If you need change this visit build.bat file.
 pause
 %msys%/mingw64.exe ./build.sh %msys%
