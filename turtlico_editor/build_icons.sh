caf01464c22100651895248353ce0fb526aab92d
saytamkenorh <saytamkenorh@seznam.cz>
2023-06-29 18:01:51 +0200

    Improve wasm webpage

--- turtlico_editor/build_icons.sh
+++ turtlico_editor/build_icons.sh
@@ -10,3 +10,13 @@ for file in "$SCRIPT_DIR/icons_raw/"*; do
             --enable-comment-stripping --shorten-ids --indent=none
     fi
 done
+
+# Favicon
+inkscape --export-type=png --export-filename="$SCRIPT_DIR/assets/favicon.png" -w 48 -h 48 "$SCRIPT_DIR/icons_raw/turtlico.svg"
+convert "$SCRIPT_DIR/assets/favicon.png" "$SCRIPT_DIR/assets/favicon.ico"
+rm "$SCRIPT_DIR/assets/favicon.png"
+
+# PWA icons
+inkscape --export-type=png --export-filename="$SCRIPT_DIR/assets/icon-256.png" -w 256 -h 256 "$SCRIPT_DIR/icons_raw/turtlico.svg"
+inkscape --export-type=png --export-filename="$SCRIPT_DIR/assets/icon-1024.png" -w 1024 -h 1024 "$SCRIPT_DIR/icons_raw/turtlico.svg"
+inkscape --export-type=png --export-filename="$SCRIPT_DIR/assets/icon_ios_touch_192" -w 192 -h 192 "$SCRIPT_DIR/icons_raw/turtlico.svg"
