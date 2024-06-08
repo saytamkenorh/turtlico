9687245357ffbccd7ae6c3a21070fc16bd977ea5
saytam <58782341+saytamkenorh@users.noreply.github.com>
2024-06-08 10:15:15 +0200

    Fix programview sense

--- turtlico_editor/src/programview.rs
+++ turtlico_editor/src/programview.rs
@@ -1,8 +1,12 @@
+use emath::{Pos2, Rect, Vec2};
 
-
-use emath::{Vec2, Pos2, Rect};
-
-use crate::{project::{Project, CommandRange, Command}, dndctl::{DnDCtl, DragAction}, app::EditorDragData, cmdrenderer::{CMD_SIZE_VEC, CMD_ICON_SIZE_VEC}, programviewdialogs};
+use crate::{
+    app::EditorDragData,
+    cmdrenderer::{CMD_ICON_SIZE_VEC, CMD_SIZE_VEC},
+    dndctl::{DnDCtl, DragAction},
+    programviewdialogs,
+    project::{Command, CommandRange, Project},
+};
 
 pub struct ProgramViewState {
     pub project: std::rc::Rc<std::cell::RefCell<Project>>,
@@ -27,11 +31,25 @@ impl ProgramViewState {
     }
 
     fn recalc_layout(&mut self, ui: &mut egui::Ui) {
-        (self.size, self.layout) = self.project.borrow().renderer.as_ref().unwrap().layout_block(
-            &self.project.borrow().program, &self.project.borrow(), ui.painter(), Pos2::new(0.0, 0.0));
+        (self.size, self.layout) = self
+            .project
+            .borrow()
+            .renderer
+            .as_ref()
+            .unwrap()
+            .layout_block(
+                &self.project.borrow().program,
+                &self.project.borrow(),
+                ui.painter(),
+                Pos2::new(0.0, 0.0),
+            );
     }
 
-    pub fn load_project(&mut self, data: &str, ctx: &egui::Context) -> Result<(), serde_json::Error> {
+    pub fn load_project(
+        &mut self,
+        data: &str,
+        ctx: &egui::Context,
+    ) -> Result<(), serde_json::Error> {
         self.set_project(std::rc::Rc::new(Project::from_str(data, ctx)?.into()));
         Ok(())
     }
@@ -46,13 +64,25 @@ impl ProgramViewState {
         {
             let (col, row) = self.xy_to_col_row(relpos);
             let mut project = self.project.borrow_mut();
-            let col_max = if row < project.program.len() { project.program[row].len() - 1 } else { 0 };
+            let col_max = if row < project.program.len() {
+                project.program[row].len() - 1
+            } else {
+                0
+            };
             let row_max = project.program.len();
-            project.insert(data.commands, usize::min(col, col_max), usize::min(row, row_max));
+            project.insert(
+                data.commands,
+                usize::min(col, col_max),
+                usize::min(row, row_max),
+            );
         }
     }
 
-    pub fn get_cmd_at_pointer(&mut self, ui: &mut egui::Ui, rect: Rect) -> Option<(crate::project::Command, CommandRange)>{
+    pub fn get_cmd_at_pointer(
+        &mut self,
+        ui: &mut egui::Ui,
+        rect: Rect,
+    ) -> Option<(crate::project::Command, CommandRange)> {
         let mut result = None;
         ui.input(|i| {
             if let Some(ptrpos) = i.pointer.interact_pos() {
@@ -60,7 +90,10 @@ impl ProgramViewState {
                 let (col, row) = self.xy_to_col_row(relpos);
                 let project = self.project.borrow();
                 if row < project.program.len() && col < project.program[row].len() {
-                    result = Some((project.program[row][col].clone(), CommandRange::single_icon(col, row)));
+                    result = Some((
+                        project.program[row][col].clone(),
+                        CommandRange::single_icon(col, row),
+                    ));
                 }
             }
         });
@@ -85,7 +118,11 @@ impl ProgramViewState {
     }
 }
 
-fn programview_ui(ui: &mut egui::Ui, state: &mut ProgramViewState, dndctl: &mut DnDCtl<EditorDragData>) -> egui::Response {  
+fn programview_ui(
+    ui: &mut egui::Ui,
+    state: &mut ProgramViewState,
+    dndctl: &mut DnDCtl<EditorDragData>,
+) -> egui::Response {
     // Layout updates
     let project_modify_timestamp = state.project.borrow().modify_timestamp;
     if project_modify_timestamp != state.project_modify_timestamp {
@@ -94,72 +131,107 @@ fn programview_ui(ui: &mut egui::Ui, state: &mut ProgramViewState, dndctl: &mut 
     }
 
     let available_space = ui.available_size();
-    let desired_size = egui::vec2(available_space.x - ui.style().spacing.item_spacing.x, available_space.y - ui.style().spacing.item_spacing.y);
+    let desired_size = egui::vec2(
+        available_space.x - ui.style().spacing.item_spacing.x,
+        available_space.y - ui.style().spacing.item_spacing.y,
+    );
     let bg_color = ui.style().visuals.extreme_bg_color;
     let fg_color = ui.style().visuals.text_color();
-    
-    ui.add_sized(desired_size, |ui: &mut egui::Ui| {
-        egui::Frame::group(ui.style()).fill(bg_color).show(ui, |ui: &mut egui::Ui| {
-            ui.set_min_size(ui.available_size());
-            egui::ScrollArea::both().show(ui, |ui| {
-                let (rect, response) = ui.allocate_at_least(
-                    Vec2::new(f32::max(ui.available_width(), state.size.width() + CMD_ICON_SIZE_VEC.x * 3.0), f32::max(ui.available_height(), state.size.height() + CMD_ICON_SIZE_VEC.y * 3.0)),
-                    egui::Sense::click_and_drag());
-                
-                // User input
-                ui.set_enabled(state.edited_cmd.is_none());
-                if ui.is_enabled() {
-                    if let Some((droppos, data)) = dndctl.drag_receive(rect) {
-                        state.insert((droppos - rect.min).to_pos2(), data);
-                    }
 
-                    if response.drag_started() {
-                        state.drag_started = Some(if response.drag_started_by(egui::PointerButton::Secondary) { DragAction::COPY } else { DragAction::MOVE });
-                    }
-                    if let Some(start_button) = state.drag_started {
-                        if response.drag_delta().length_sq() > 3.0 {
-                            if let Some((cmd, range)) = state.get_cmd_at_pointer(ui, rect) {
-                                dndctl.drag_start(ui, EditorDragData {
-                                    action: start_button,
-                                    commands: vec![vec![cmd]],
-                                    commands_range: Some(range),
-                                    project: state.project.clone(),
-                                });
+    ui.add_sized(desired_size, |ui: &mut egui::Ui| {
+        egui::Frame::group(ui.style())
+            .fill(bg_color)
+            .show(ui, |ui: &mut egui::Ui| {
+                ui.set_min_size(ui.available_size());
+                egui::ScrollArea::both().show(ui, |ui| {
+                    let (rect, response) = ui.allocate_at_least(
+                        Vec2::new(
+                            f32::max(
+                                ui.available_width(),
+                                state.size.width() + CMD_ICON_SIZE_VEC.x * 3.0,
+                            ),
+                            f32::max(
+                                ui.available_height(),
+                                state.size.height() + CMD_ICON_SIZE_VEC.y * 3.0,
+                            ),
+                        ),
+                        egui::Sense::click_and_drag(),
+                    );
+
+                    // User input
+                    ui.set_enabled(state.edited_cmd.is_none());
+                    if ui.is_enabled() {
+                        if let Some((droppos, data)) = dndctl.drag_receive(rect) {
+                            state.insert((droppos - rect.min).to_pos2(), data);
+                        }
+
+                        if response.drag_started() {
+                            state.drag_started = Some(
+                                if response.drag_started_by(egui::PointerButton::Secondary) {
+                                    DragAction::COPY
+                                } else {
+                                    DragAction::MOVE
+                                },
+                            );
+                        }
+                        if let Some(start_button) = state.drag_started {
+                            if response.drag_delta().length_sq() > 3.0 {
+                                if let Some((cmd, range)) = state.get_cmd_at_pointer(ui, rect) {
+                                    dndctl.drag_start(
+                                        ui,
+                                        EditorDragData {
+                                            action: start_button,
+                                            commands: vec![vec![cmd]],
+                                            commands_range: Some(range),
+                                            project: state.project.clone(),
+                                        },
+                                    );
+                                }
+                                state.drag_started = None;
                             }
+                        }
+                        if !response.dragged() {
                             state.drag_started = None;
                         }
-                    }
-                    if !response.dragged() {
-                        state.drag_started = None;
-                    }
 
-                    if response.secondary_clicked() {
-                        if let Some((cmd, range)) = state.get_cmd_at_pointer(ui, rect) {
-                            state.edited_cmd = Some((range.start.0, range.start.1, cmd.clone()));
+                        if response.secondary_clicked() {
+                            if let Some((cmd, range)) = state.get_cmd_at_pointer(ui, rect) {
+                                state.edited_cmd =
+                                    Some((range.start.0, range.start.1, cmd.clone()));
+                            }
                         }
                     }
-                }
 
-                programviewdialogs::dialog(ui, state);
+                    programviewdialogs::dialog(ui, state);
 
-                if ui.is_rect_visible(rect) {
-                    let visuals = ui.style().noninteractive();
-                    let rect = rect.expand(visuals.expansion);
-                    let painter = ui.painter().with_clip_rect(rect);
-            
-                    {
-                        let mut project = state.project.borrow_mut();
-                        project.renderer.as_mut().unwrap().color_program_bg = bg_color;
-                        project.renderer.as_mut().unwrap().color_program_fg = fg_color;
+                    if ui.is_rect_visible(rect) {
+                        let visuals = ui.style().noninteractive();
+                        let rect = rect.expand(visuals.expansion);
+                        let painter = ui.painter().with_clip_rect(rect);
+
+                        {
+                            let mut project = state.project.borrow_mut();
+                            project.renderer.as_mut().unwrap().color_program_bg = bg_color;
+                            project.renderer.as_mut().unwrap().color_program_fg = fg_color;
+                        }
+                        let project = state.project.borrow();
+                        project.renderer.as_ref().unwrap().render_block(
+                            &project.program,
+                            &project,
+                            &painter,
+                            rect.min,
+                            None,
+                        );
                     }
-                    let project = state.project.borrow();
-                    project.renderer.as_ref().unwrap().render_block(&project.program, &project, &painter, rect.min, None);
-                }
-            });
-        }).response
-    }).interact(egui::Sense::drag())
+                });
+            })
+            .response
+    })
 }
 
-pub fn programview<'a>(state: &'a mut ProgramViewState, dndctl: &'a mut DnDCtl<EditorDragData>) -> impl egui::Widget + 'a {
+pub fn programview<'a>(
+    state: &'a mut ProgramViewState,
+    dndctl: &'a mut DnDCtl<EditorDragData>,
+) -> impl egui::Widget + 'a {
     move |ui: &mut egui::Ui| programview_ui(ui, state, dndctl)
 }
